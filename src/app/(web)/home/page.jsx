659938371b06548165e5ffcbab181613892fad03
Deny Celestino <denycelestino21@gmail.com>
2023-10-23 10:23:25 +0200

    sounds

--- src/app/(web)/home/page.jsx
+++ src/app/(web)/home/page.jsx
@@ -12,6 +12,8 @@ import { useEffect, useState } from 'react'
 export default function Home() {
   const { modalInstall, setModalInstall } = useMyContext()
   const [deferredPrompt, setDeferredPrompt] = useState(null)
+  const [iosInstalled, setIosInstalled] = useState(false)
+  const [androidInstalled, setAndroidInstalled] = useState(false)
   let isiOS = false // Inicialize a variável isiOS
 
   if (typeof window !== 'undefined') {
@@ -21,6 +23,38 @@ export default function Home() {
     // Resto do seu código que depende de 'navigator'
   }
   useEffect(() => {
+    if (
+      'standalone' in window.navigator &&
+      window.navigator.standalone
+    ) {
+      setIosInstalled(true)
+      // A PWA está instalada no dispositivo iOS (Safari).
+    } else {
+      setIosInstalled(false)
+      // A PWA não está instalada no dispositivo iOS.
+    }
+    if (window.navigator.getInstalledRelatedApps) {
+      window.navigator
+        .getInstalledRelatedApps()
+        .then(relatedApps => {
+          if (relatedApps.length > 0) {
+            setAndroidInstalled(true)
+            // A PWA está instalada no dispositivo Android.
+          } else {
+            setAndroidInstalled(false)
+            // A PWA não está instalada no dispositivo Android.
+          }
+        })
+        .catch(error => {
+          console.error(
+            'Erro ao verificar as apps relacionadas:',
+            error
+          )
+        })
+    } else {
+      // A propriedade getInstalledRelatedApps não é suportada no navegador.
+    }
+
     const handleBeforeInstallPrompt = event => {
       // Armazena o evento para ser usado posteriormente
       event.preventDefault()
@@ -60,60 +94,71 @@ export default function Home() {
   }
   return (
     <div className="flex items-center bg-zinc-50 justify-center min-h-screen text-xs md:text-base">
-      {modalInstall && (
-        <AnimatePresence>
-          <motion.div
-            initial={{ opacity: 0, y: 200 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: 200 }}
-            transition={{ duration: 0.8 }}
-            className="bg-white shadow-2xl p-4 rounded-lg fixed bottom-0 left-0 right-0  z-20 flex items-center gap-4 text-xs md:text-base"
-          >
-            <div className="h-10 w-10">
-              <Image
-                className="h-full w-full"
-                src={LOGO}
-                alt="logo"
-              />
-            </div>
-            <div className="flex flex-col gap-1">
-              <span>Instale o driver no seu disposito.</span>
-              <div className="flex flex-col gap-2">
-                {isiOS ? (
-                  <div>
-                    <p>
-                      Adicione este aplicativo à sua tela inicial:
-                    </p>
-                    <p>
-                      1. Toque no ícone de compartilhamento no
-                      navegador.
-                    </p>
-                    <p>2. Selecione Adicionar à Tela Inicial.</p>
+      {!iosInstalled ||
+        (!androidInstalled && (
+          <>
+            {modalInstall && (
+              <AnimatePresence>
+                <motion.div
+                  initial={{ opacity: 0, y: 200 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: 200 }}
+                  transition={{ duration: 0.8 }}
+                  className="bg-white shadow-2xl p-4 rounded-lg fixed bottom-0 left-0 right-0  z-20 flex items-center gap-4 text-xs md:text-base"
+                >
+                  <div className="h-10 w-10">
+                    <Image
+                      className="h-full w-full"
+                      src={LOGO}
+                      alt="logo"
+                    />
                   </div>
-                ) : (
-                  <div>
-                    <button
-                      onClick={handleInstallClick}
-                      className="py-2 px-4 rounded bg-green-400 text-white"
-                    >
-                      Instalar agora
-                    </button>
-                  </div>
-                )}
+                  <div className="flex flex-col gap-1">
+                    <span>Instale o driver no seu disposito.</span>
+                    <div className="flex flex-col gap-2">
+                      {isiOS ? (
+                        <div>
+                          <p>
+                            Adicione este aplicativo à sua tela
+                            inicial:
+                          </p>
+                          <p>
+                            1. Toque no ícone de compartilhamento no
+                            navegador.
+                          </p>
+                          <p>
+                            2. Selecione Adicionar à Tela Inicial.
+                          </p>
+                        </div>
+                      ) : (
+                        <div>
+                          <button
+                            onClick={handleInstallClick}
+                            className="py-2 px-4 rounded bg-green-400 text-white"
+                          >
+                            Instalar agora
+                          </button>
+                        </div>
+                      )}
 
-                <div>
-                  <button
-                    onClick={() => setModalInstall(!modalInstall)}
-                    className="py-2 px-4 rounded bg-transparent"
-                  >
-                    Cancelar
-                  </button>
-                </div>
-              </div>
-            </div>
-          </motion.div>
-        </AnimatePresence>
-      )}
+                      <div>
+                        <button
+                          onClick={() =>
+                            setModalInstall(!modalInstall)
+                          }
+                          className="py-2 px-4 rounded bg-transparent"
+                        >
+                          Cancelar
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </motion.div>
+              </AnimatePresence>
+            )}
+          </>
+        ))}
+
       <div className="wrapper flex flex-col gap-2">
         <h1 className="text-2xl md:text-3xl">LOGO</h1>
         <p className="text-xs md:text-base w-10/12 md:w-full">
