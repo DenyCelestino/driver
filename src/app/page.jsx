1025dfe74132e894cdb8cbd109c5e64b51e96ca1
Deny Celestino <denycelestino21@gmail.com>
2023-11-06 23:27:25 +0200

    updates

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,12 +2,26 @@
 
 import Header from '@/components/LandingPage/Header'
 import Hero from '@/components/LandingPage/Hero'
+import { ContextUser } from '@/context/ContextUser'
+import Cookies from 'js-cookie'
 
 import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 
 export default function Main() {
+  const { setUser } = ContextUser()
   const router = useRouter()
 
+  useEffect(() => {
+    const userData = Cookies.get('user')
+    if (Cookies.get('logged')) {
+      if (!userData) {
+        router.push('/login')
+      } else {
+        router.push('/dashboard')
+      }
+    }
+  }, [router])
   return (
     <>
       <Header />
