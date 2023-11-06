a6aeb20bb51f9aeaf6afb4d19403787801b64e13
Deny Celestino <denycelestino21@gmail.com>
2023-11-06 17:01:00 +0200

    heii

--- src/functions/PrivateRoutes.jsx
+++ src/functions/PrivateRoutes.jsx
@@ -13,50 +13,50 @@ export default function PrivateRoutes({ children }) {
 
   const router = useRouter()
 
-  const checkPlan = async user => {
-    try {
-      setLoadingCheckPlan(true)
-      let res = await axios.get(
-        `${ENDPOINT}checkdays.php?user=${user.id}`
-      )
-      console.log(res.data)
-      setBypass(res.data)
+  // const checkPlan = async user => {
+  //   try {
+  //     setLoadingCheckPlan(true)
+  //     let res = await axios.get(
+  //       `${ENDPOINT}checkdays.php?user=${user.id}`
+  //     )
+  //     console.log(res.data)
+  //     setBypass(res.data)
 
-      if (res.data.status == 200) {
-        router.push('/dashboard')
-        setLoadingCheckPlan(false)
-      } else if (res.data.status == 404) {
-        router.push('/payment')
-        setLoadingCheckPlan(false)
-      } else {
-        setLoadingCheckPlan(false)
-        toast.error(
-          'Opps, erro tentando verificar o plano, tente novamente'
-        )
-        router.push('/login')
-      }
-    } catch (error) {
-      console.error('Erro ao verificar o plano:', error)
-      router.push('/login')
-    }
-  }
+  //     if (res.data.status == 200) {
+  //       router.push('/dashboard')
+  //       setLoadingCheckPlan(false)
+  //     } else if (res.data.status == 404) {
+  //       router.push('/payment')
+  //       setLoadingCheckPlan(false)
+  //     } else {
+  //       setLoadingCheckPlan(false)
+  //       toast.error(
+  //         'Opps, erro tentando verificar o plano, tente novamente'
+  //       )
+  //       router.push('/login')
+  //     }
+  //   } catch (error) {
+  //     console.error('Erro ao verificar o plano:', error)
+  //     router.push('/login')
+  //   }
+  // }
 
-  useEffect(() => {
-    const userData = Cookies.get('user')
-    if (Cookies.get('logged')) {
-      if (!userData) {
-        console.log(userData)
-        console.log('no user')
-        router.push('/login')
-      } else {
-        console.log('user setado: ', JSON.parse(userData))
-        setUser(JSON.parse(userData))
-        checkPlan(JSON.parse(userData))
-      }
-    } else {
-      router.push('/login')
-    }
-  }, [router, setUser, checkPlan])
+  // useEffect(() => {
+  //   const userData = Cookies.get('user')
+  //   if (Cookies.get('logged')) {
+  //     if (!userData) {
+  //       console.log(userData)
+  //       console.log('no user')
+  //       router.push('/login')
+  //     } else {
+  //       console.log('user setado: ', JSON.parse(userData))
+  //       setUser(JSON.parse(userData))
+  //       checkPlan(JSON.parse(userData))
+  //     }
+  //   } else {
+  //     router.push('/login')
+  //   }
+  // }, [router, setUser, checkPlan])
 
   return user ? children : null
 }
