0e301528196b5e2beaf864ed2b510fab2d542465
Deny Celestino <denycelestino21@gmail.com>
2023-11-06 09:39:00 +0200

    loading screen

--- src/app/(app)/loading.jsx
+++ src/app/(app)/loading.jsx
@@ -1,9 +1,7 @@
 export default function Loading() {
   return (
     <div className="loading-screen">
-      <h1>
-        <Carregando styleName=""></Carregando>
-      </h1>
+      <h1>Carregando ...</h1>
     </div>
   )
 }
