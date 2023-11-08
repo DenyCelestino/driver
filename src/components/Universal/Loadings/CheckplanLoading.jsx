d48bbb2acbd2d9fc21e237edf7bec08e11716421
Deny Celestino <denycelestino21@gmail.com>
2023-11-08 16:09:54 +0200

    new uploads

--- src/components/Universal/Loadings/CheckplanLoading.jsx
+++ src/components/Universal/Loadings/CheckplanLoading.jsx
@@ -1,5 +1,3 @@
-import Lottie from 'lottie-react'
-import SCAN from '../../../../public/animations/scan.json'
 import { FadeLoader } from 'react-spinners'
 export default function CheckplanLoading() {
   return (
