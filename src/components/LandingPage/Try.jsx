04d517ac81d401c048cc7bbe14a6176779473c3f
Deny Celestino <denycelestino21@gmail.com>
2023-11-14 16:17:11 +0200

    landing pages

--- src/components/LandingPage/Try.jsx
+++ src/components/LandingPage/Try.jsx
@@ -1,9 +1,21 @@
 "use client";
 
+import Link from "next/link";
+
 export default function Try() {
   return (
-    <div>
-      <h1>Try</h1>
-    </div>
+    <section className="try">
+      <div className="wrapper-website">
+        <div className="content">
+          <span>
+            Primeira Tentativa, <br />{" "}
+            <span className="bold"> Resultado Perfeito</span>
+          </span>
+          <div>
+            <Link href={"/free"}>Comece agora gratis</Link>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
