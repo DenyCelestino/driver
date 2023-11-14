f99ff73af2fab7ce48c60a6a55a084cc75dd3563
Deny Celestino <denycelestino21@gmail.com>
2023-11-14 17:27:08 +0200

    landing pages

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,14 +7,61 @@ import Start from "@/components/LandingPage/Start";
 import Testemonials from "@/components/LandingPage/Testemonials";
 import Try from "@/components/LandingPage/Try";
 import Why from "@/components/LandingPage/Why";
+import { useMyContext } from "@/context/Context";
 import { ContextUser } from "@/context/ContextUser";
 import Cookies from "js-cookie";
 
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Main() {
   const { setUser } = ContextUser();
+  const { modalInstall, setModalInstall } = useMyContext();
+  const [deferredPrompt, setDeferredPrompt] = useState(null);
+  const [installed, setInstalled] = useState(true);
+  let isiOS = false; // Inicialize a variável isiOS
+
+  if (typeof window !== "undefined") {
+    isiOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+
+    // Resto do seu código que depende de 'navigator'
+  }
+  useEffect(() => {
+    const handleBeforeInstallPrompt = (event) => {
+      // Armazena o evento para ser usado posteriormente
+      event.preventDefault();
+
+      setInstalled(false);
+      setDeferredPrompt(event);
+    };
+
+    window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
+
+    return () => {
+      window.removeEventListener(
+        "beforeinstallprompt",
+        handleBeforeInstallPrompt
+      );
+    };
+  }, []);
+
+  const handleInstallClick = () => {
+    if (deferredPrompt) {
+      // Inicia a instalação
+      deferredPrompt.prompt();
+
+      deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === "accepted") {
+          console.log("O usuário aceitou a instalação da PWA");
+        } else {
+          console.log("O usuário cancelou a instalação da PWA");
+        }
+
+        // Limpa o evento para que não possa ser usado novamente
+        setDeferredPrompt(null);
+      });
+    }
+  };
   const router = useRouter();
 
   useEffect(() => {
