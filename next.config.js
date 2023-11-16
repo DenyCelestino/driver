eab7e92e78a19ff1a2db5d4d45f5672bee012dfa
Deny Celestino <denycelestino21@gmail.com>
2023-11-16 14:25:49 +0200

    backup with backend

--- next.config.js
+++ next.config.js
@@ -1,14 +1,32 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')
+const withPWA = require("next-pwa");
 const nextConfig = {
   ...withPWA({
-    dest: 'public',
+    dest: "public",
     register: true,
-    skipWaiting: true
+    skipWaiting: true,
   }),
   httpAgentOptions: {
-    keepAlive: true
-  }
-}
+    keepAlive: true,
+  },
+};
+// next.config.js
 
-module.exports = nextConfig
+module.exports = {
+  async rewrites() {
+    return [
+      // Roteamento para o frontend
+      {
+        source: "/:path*",
+        destination: "https://trafegotop.app/:path*", // Substitua pelo endereço correto do seu frontend
+      },
+      // Roteamento para o backend (subdomínio api)
+      {
+        source: "/trafegotop/api/:path*",
+        destination: "https://api.trafegotop.app/:path*", // Substitua pelo endereço correto do seu backend
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
