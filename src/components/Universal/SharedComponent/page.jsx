bc2d6efb34246b301e576f7943d6bb5581dcb86b
Deny Celestino <denycelestino21@gmail.com>
2023-11-17 11:08:46 +0200

    whatsapp link

--- src/components/Universal/SharedComponent/page.jsx
+++ src/components/Universal/SharedComponent/page.jsx
@@ -16,14 +16,17 @@ export default function SharedComponent({ info }) {
         <h3>{info.user} passou no exame de condução preparatorio.</h3>
         <p>Você pode se preparar para o exame tambem, clique em:</p>
 
-        <Link href={"/"}>Saber mais</Link>
+        <Link className="link" href={"/"}>
+          Saber mais
+        </Link>
         <FacebookShareButton url={`https://trafegotop.app/shared/${info.slug}`}>
           <button className="button facebook">
             Partilhar com amigos no facebook
           </button>
         </FacebookShareButton>
         <Link
-          href={`https://trafegotop.app/shared/${info.slug}`}
+          target="_blank"
+          href={`https://api.whatsapp.com/send/?text=https://trafegotop.app/shared/${info.slug}`}
           className="button whatsapp"
         >
           Partilhar com amigos no Whatsapp
